343b154e009ea754b707f9501beee60e6c3f0f53
sandras1 <sandra@techversantinfo.com>
2024-01-16 16:03:54 +0530

    Modified Views

--- recommend/views.py
+++ recommend/views.py
@@ -4,41 +4,45 @@ from rest_framework import status
 import pandas as pd
 import random as rand
 import json
+from django.http import JsonResponse
 
 class GetRecommendationsAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            try:
-                request_json = request.data
-                ass_score = request_json["score"]
-                ass_name = request_json["courseName"]
-                stu_id = request_json["userId"]
-                req_id = request_json["requestId"]
-                asse_id = request_json["courseId"]
-            except KeyError as e:
-                # Handle the case where a required key is missing in the JSON data
-                error_message = f"KeyError: '{e.args[0]}' is missing in the JSON Data"
-                return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
-            df1 = pd.read_csv("sheet_1.csv")
-            df2 = pd.read_csv("sheet_3.csv")
+        try:
+            request_json = request.data
+            ass_score = request_json["score"]
+            ass_name = request_json["courseName"]
+            stu_id = request_json["userId"]
+            req_id = request_json["requestId"]
+            asse_id = request_json["courseId"]
+        except KeyError as e:
+            error_message = f"KeyError: '{e.args[0]}' is missing in the JSON Data"
+            return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
 
-            cate_assese = df1[(df1['from'] <= ass_score) & (df1['to'] >= ass_score)][['category', 'assesment_type']]
-            category = cate_assese['category'].values[0]
-            ass_type = cate_assese['assesment_type'].values[0]
-            prodt_id = df2[(df2['category'] == category) & (df2['assesment_type'] == ass_type) & (
-                        df2['assesment_name'] == ass_name)][['product_id', 'product_name']]
-            product_dict = dict(zip(prodt_id.product_id, prodt_id.product_name))
-            final_list = []
-            lis = product_dict.keys()
-            program_id = self.wheel(list(lis))
-            for i in program_id:
-                new = product_dict[i]
-                data = {"programId": i, "programName": new}
-                final_list.append(data)
-            send_asses = {"userId": stu_id, "courseId": asse_id, "courseName": ass_name,
-                        "assessmentType": ass_type, "category": category, "requestId": req_id,
-                        "recommended_programs": final_list}
-            return Response(send_asses, status=status.HTTP_200_OK)
+        df1 = pd.read_csv("sheet_1.csv")
+        df2 = pd.read_csv("sheet_3.csv")
+
+        cate_assese = df1[(df1['from'] <= ass_score) & (df1['to'] >= ass_score)][['category', 'assesment_type']]
+        category = cate_assese['category'].values[0]
+        ass_type = cate_assese['assesment_type'].values[0]
+        prodt_id = df2[(df2['category'] == category) & (df2['assesment_type'] == ass_type) & (
+                    df2['assesment_name'] == ass_name)][['product_id', 'product_name', 'product_description']]
+
+        product_dict = dict(zip(prodt_id.product_id, prodt_id.product_name))
+        product_desc_dict = dict(zip(prodt_id.product_id, prodt_id.product_description))
+        final_list = []
+        lis = product_dict.keys()
+        program_id = self.wheel(list(lis))
+        for i in program_id:
+            new = product_dict[i]
+            desc = product_desc_dict[i]
+            data = {"programId": i, "programName": new, "programDescription": desc}
+            final_list.append(data)
+
+        send_asses = {"userId": stu_id, "courseId": asse_id, "courseName": ass_name,
+                      "assessmentType": ass_type, "category": category, "requestId": req_id,
+                      "recommended_programs": final_list}
+        return Response(send_asses, status=status.HTTP_200_OK)
 
     def roul_wheel(self, wheel_array):
         arrsize = len(wheel_array)
