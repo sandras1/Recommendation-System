54da99cab8f250d5b05dde09685af51454c13ad4
sandras1 <sandra@techversantinfo.com>
2024-01-16 14:53:49 +0530

    Modified

--- RecommendationSystem/settings.py
+++ RecommendationSystem/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'RecommendationSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
